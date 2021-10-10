7183c8b7cebd471a002828506d89ee1bf4d6134d
Bharat Kumar <kumarbharat661@gmail.com>
2021-10-10 22:39:37 +0530

    added the time table

--- src/components/Dashboard/TimeTable/TimeTable.jsx
+++ src/components/Dashboard/TimeTable/TimeTable.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import timeTable from '../../../assets/TimeTable.jpg';
+import '../../../styles/Dashboard/TimeTable.scss';
 
 const TimeTable = () => {
-  return <div className="wrapper">timetable</div>;
+  return (
+    <div className="wrapper">
+      <div className="timetable">
+        <img src={timeTable} width="1000px" height="500px" alt="Time Table" />
+      </div>
+    </div>
+  );
 };
 
 export default TimeTable;
