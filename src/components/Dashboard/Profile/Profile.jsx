01dd33988b6f6e865bd1f5abc9b7ba94e6fa93df
Bharat Kumar <kumarbharat661@gmail.com>
2021-10-10 14:16:45 +0530

    Made the Profile page

--- src/components/Dashboard/Profile/Profile.jsx
+++ src/components/Dashboard/Profile/Profile.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import profile from '../../../assets/Profile.png';
+import '../../../styles/Dashboard/Profile.scss';
 
 const Profile = () => {
-  return <div className="wrapper">profile</div>;
+  return (
+    <div className="wrapper">
+      <div className="profileWrapper">
+        <div className="imgContainer">
+          <img src={profile} alt="logo" width="200px" height="200px" />
+          <h4>2020 - 2024</h4>
+          <h6>IIIT - Sonepat</h6>
+        </div>
+        <div className="userDataContainer">
+          <ul>
+            <li>
+              <label>Name</label>
+              <label>Bharat Kumar</label>
+            </li>
+            <li>
+              <label>Roll</label>
+              <label>1201105264</label>
+            </li>
+            <li>
+              <label>Course</label>
+              <label>B.Tech (CSE)</label>
+            </li>
+            <li>
+              <label>DOB</label>
+              <label>15/08/2001</label>
+            </li>
+            <li>
+              <label>Phone No.</label>
+              <label>9711082565</label>
+            </li>
+            <li>
+              <label>Email</label>
+              <label>kumabharat123@gmail.com</label>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
