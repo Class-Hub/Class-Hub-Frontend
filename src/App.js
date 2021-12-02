388cee630be40c421e905520fa768c847cd6f183
Bharat Kumar <kumarbharat661@gmail.com>
2021-12-03 00:56:18 +0530

    Made the dark mode switch

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import Dashboard from './pages/Dashboard/Index';
 import { ProfileProvider } from './context/profile.context';
 import PublicRoute from './components/Routehandler/PublicRoute';
 import PrivateRoute from './components/Routehandler/PrivateRoute';
-import { ThemeProvider } from 'styled-components';
 
-const theme = {
-  primary: '#E6E6E6',
-  secondary: '#5C7AEA',
-  accent1: '#3D56B2',
-  accent2: '#14279B',
-  font_primary: '#000000',
-  font_secondary: '#150E56',
-};
 
 function App() {
   return (
@@ -26,11 +17,9 @@ function App() {
         <PublicRoute path="/signin">
           <LandingPage />
         </PublicRoute>
-        <ThemeProvider theme={theme}>
           <PrivateRoute path="/dashboard">
             <Dashboard />
           </PrivateRoute>
-        </ThemeProvider>
       </Switch>
     </ProfileProvider>
   );
